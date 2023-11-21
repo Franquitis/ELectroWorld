34340c7ac1f4de42c767500774cb1dadc6f7398f
Franquitis <110854980+Franquitis@users.noreply.github.com>
2023-11-21 17:08:21 -0300

    Add files via upload

--- src/app/secciones/servicios/servicios.component.ts
+++ src/app/secciones/servicios/servicios.component.ts
@@ -19,7 +19,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 7, name: 'PS5 Estandar God of War', weight: 'r', symbol: '$891.500', categoria:'Tecnologia'},
   {position: 8, name: 'PS4 1TB', weight: 'r', symbol: '$665.700', categoria:'Tecnologia'},
   {position: 9, name: 'Notebook Lenovo Gamer 15,6', weight: 'r', symbol: '$899.999', categoria:'Tecnologia'},
-  {position: 10, name: 'Notebook Delll 15,6', weight: 'r', symbol: '$549.999"', categoria:'Tecnologia'},
+  {position: 10, name: 'Notebook Delll 15,6', weight: 'r', symbol: '$549.999', categoria:'Tecnologia'},
   {position: 12, name: 'iPhone 13 128GB Starlight', weight: 'r', symbol: '$2.377.500', categoria:'Tecnologia'},
   {position: 13, name: 'Celular Samsung Galaxy A54', weight: 'r', symbol: '$289.999', categoria:'Tecnologia'},
   {position: 14, name: 'Parlante Bluetooth Philco', weight: 'r', symbol: '$29.999', categoria:'TV y Audio'},
